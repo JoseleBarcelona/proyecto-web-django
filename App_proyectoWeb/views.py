bcdb791833dc32521ececffa5d275a62b04af7d1
JoseleBarcelona <124534590+JoseleBarcelona@users.noreply.github.com>
2024-01-20 09:28:23 +0100

    Agregar el proyecto, la app y un .gitignore al repositorio

--- App_proyectoWeb/views.py
+++ App_proyectoWeb/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def inicio(request):
+
+    return HttpResponse("Inicio")
+
+def servicios(request):
+
+    return HttpResponse("Servicios")
+
+def tienda(request):
+
+    return HttpResponse("Tienda")
+
+def blog(request):
+
+    return HttpResponse("Blog")
+
+def contacto(request):
+
+    return HttpResponse("Contacto")
--- proyectoWeb/urls.py
+++ proyectoWeb/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from App_proyectoWeb import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.inicio, name='inicio'),
+    path('servicios/', views.servicios, name='servicios'),
+    path('tienda/', views.tienda, name='tienda'),
+    path('blog/', views.blog, name='blog'),
+    path('contacto/', views.contacto, name='contacto'),
 ]
