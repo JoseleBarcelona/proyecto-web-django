1ffca37b806a4c8f3ebbf884856203a9c44c223d
JoseleBarcelona <124534590+JoseleBarcelona@users.noreply.github.com>
2024-01-24 21:01:15 +0100

    Conf:Configurar Django para que renderize imágenes

--- App_proyectoWeb/urls.py
+++ App_proyectoWeb/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from App_proyectoWeb import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -8,3 +10,5 @@ urlpatterns = [
     path('blog/', views.blog, name='blog'),
     path('contacto/', views.contacto, name='contacto'),
 ]
+
+urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
