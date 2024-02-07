4e9b10fd10e039036ddc0acf004b3088c45728f4
JoseleBarcelona <124534590+JoseleBarcelona@users.noreply.github.com>
2024-02-07 23:35:25 +0100

    Added rendered filters about categorias

--- App_blog/urls.py
+++ App_blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 
     path('', views.blog, name='blog'),
+    path('categoria/<int:categoria_id>/', views.categoria, name='categoria'),
 
 ]
--- App_blog/views.py
+++ App_blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from App_blog.models import Post
+from App_blog.models import Post, Categoria
 
 # Create your views here.
 
@@ -7,3 +7,9 @@ def blog(request):
     
     post = Post.objects.all()
     return render(request, 'App_blog/blog.html', {'posts': post})
+
+def categoria(request, categoria_id):
+
+    categoria = Categoria.objects.get(id=categoria_id)
+    post = Post.objects.filter(categorias=categoria)
+    return render(request, 'App_blog/categorias.html', {'categoria': categoria, 'posts': post})
