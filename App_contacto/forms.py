776b0ab5957f4a31bb0bc30b3b5f63943a6eb126
JoseleBarcelona <124534590+JoseleBarcelona@users.noreply.github.com>
2024-02-17 22:19:22 +0100

    Agregar estilos a la app de contactos

--- App_contacto/forms.py
+++ App_contacto/forms.py
@@ -5,4 +5,4 @@ class FormularioContacto(forms.Form):
 
     nombre = forms.CharField(label="Nombre", required=True)
     email = forms.EmailField(label="Email", required=True)
-    contenido = forms.CharField(label="Contenido")
+    contenido = forms.CharField(label="Contenido", widget=forms.Textarea)
