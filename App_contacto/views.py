7c51f62f131b6364eb3440dd20b453772fc10bee
JoseleBarcelona <124534590+JoseleBarcelona@users.noreply.github.com>
2024-02-20 23:38:53 +0100

    Capturar en variables los datos insertados por el usuario

--- App_contacto/views.py
+++ App_contacto/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import FormularioContacto
 
 # Create your views here.
@@ -6,4 +6,14 @@ from .forms import FormularioContacto
 def contacto(request):
 
     formulario_contacto = FormularioContacto()
+
+    if request.method == 'POST':
+        formulario_contacto = FormularioContacto(data=request.POST)
+        if formulario_contacto.is_valid():
+            nombre = request.POST.get('nombre')
+            email = request.POST.get('email')
+            contenido = request.POST.get('contenido')
+
+            return redirect('/contacto/?valido')
+
     return render(request, 'contacto/contacto.html', {'miFormulario': formulario_contacto})
