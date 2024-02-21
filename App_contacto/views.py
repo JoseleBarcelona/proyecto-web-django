fefe3a0992f470590a5ea7ad27d08252ff9dd5b5
JoseleBarcelona <124534590+JoseleBarcelona@users.noreply.github.com>
2024-02-21 20:25:15 +0100

    Configurar email para enviar correos desde Django

--- App_contacto/views.py
+++ App_contacto/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from .forms import FormularioContacto
+from django.core.mail import EmailMessage
 
 # Create your views here.
 
 def contacto(request):
 
-    formulario_contacto = FormularioContacto()
+    formulario_contacto = FormularioContacto( )
 
     if request.method == 'POST':
         formulario_contacto = FormularioContacto(data=request.POST)
@@ -14,6 +15,17 @@ def contacto(request):
             email = request.POST.get('email')
             contenido = request.POST.get('contenido')
 
-            return redirect('/contacto/?valido')
+            email = EmailMessage('Mensaje desde la App de Django',
+            'El ususario con nombre {} con la dirección {}, escribe lo siguiente: \n\n{}'.format(nombre, email, contenido),
+            '', ['correo_del_remitente@gmail.com'], reply_to=[email])
+
+            try:
+                email.send()
+
+                return redirect('/contacto/?valido')
+            
+            except:
+                return redirect('/contacto/?novalido')
+
 
     return render(request, 'contacto/contacto.html', {'miFormulario': formulario_contacto})
--- proyectoWeb/settings.py
+++ proyectoWeb/settings.py
@@ -129,3 +129,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuración de email
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'correo_del_administrador@gmail.com'
+EMAIL_HOST_PASSWORD = 'contraseña del correo del administrador'
